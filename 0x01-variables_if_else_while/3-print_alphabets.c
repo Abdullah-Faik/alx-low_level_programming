8c9abb024b5061c946e86cf44d5ad94493e78751
abdullah <abdallahfaik80@gmail.com>
2023-03-17 21:03:46 +0200

    task number 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - ptiny alpha
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char start = 'a';
+
+	while (start <= 'z')
+	{
+		putchar(start);
+		start += 1;
+	}
+	start = 'A'
+	while (start <= 'Z')
+	{
+		putchar(start);
+		start += 1;
+	}
+	putchar('\n');
+
+	return (0);
+}
