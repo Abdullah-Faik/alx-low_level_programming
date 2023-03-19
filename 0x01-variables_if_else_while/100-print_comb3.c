c593e7634469cc76d6e17578e0fdebb1230fdcea
abdullah <abdallahfaik80@gmail.com>
2023-03-19 21:57:45 +0200

    task number 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+	int i = 0;
+	int j = 0;
+
+	while (i < 8)
+	{
+		j = i + 1;
+		while (j < 9)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i == 8 && j == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
