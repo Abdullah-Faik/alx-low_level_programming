148a4fa9bdade2fb270341e1951ae1e897c7a0dd
abdullah <abdallahfaik80@gmail.com>
2023-03-19 22:41:05 +0200

    task number 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+	int i = 0;
+	int k = 0;
+	int j = 0;
+        int l = 0;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 9)
+		{
+			k = 0;
+			while (k < 10)                        
+			{
+				l = 0;
+				while (l <10)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (i == 9 && j == 8 && k == 9 && l == 9)
+						break;
+					putchar(',');
+					putchar(' ');
+					l++;
+				}
+				k++;
+
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
