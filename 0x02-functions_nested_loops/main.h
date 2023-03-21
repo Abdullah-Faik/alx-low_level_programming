8b2b4a15adabd04324496031e47a310c2301ff77
abdullah <abdallahfaik80@gmail.com>
2023-03-21 21:33:24 +0200

    task number 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,12 @@
-#pragma once
+#ifndef MAIN_H
+#define MAIN_H
+void _putchar(char x);
+
+#endif
+
 #include <stdio.h>
 /**
- * _putchar - printing 
+ * _putchar - printing
  * @x :  recive value to print
 */
 void _putchar(char x)
