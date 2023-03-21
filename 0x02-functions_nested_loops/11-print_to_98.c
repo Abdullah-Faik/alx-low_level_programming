fb06353b75ce43d5c40eb5d43b216a3dc0fa2f3d
abdullah <abdallahfaik80@gmail.com>
2023-03-22 01:44:01 +0200

    task number 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_to_98 - print to 98
+ * @n: recive number
+*/
+void print_to_98(int n)
+{
+	while (n < 99)
+	{
+
+		printf("%d", n);
+		if (n == 98)
+			break;
+		prinf(", ");
+		n++;
+	}
+}
