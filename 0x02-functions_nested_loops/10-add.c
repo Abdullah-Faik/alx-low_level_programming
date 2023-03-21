7d59373053fca1be7bc0296c183c34f19a1a58f4
abdullah <abdallahfaik80@gmail.com>
2023-03-22 01:35:17 +0200

    task number 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - addition
+ * @n: first oper
+ * @l: second operator
+ * Return: 1 if the character is lowercase, 0 otherwise
+ */
+int add(int n, int l)
+{
+	return (n + l);
+}
