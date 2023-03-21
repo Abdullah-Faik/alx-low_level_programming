9e6c0931f0c107928287c50bc87c3b45230d6ff2
abdullah <abdallahfaik80@gmail.com>
2023-03-21 21:15:31 +0200

    task number 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print word
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char i[] = "_putchar";
+	int counter = 0;
+
+	while (counter < 8)
+	{
+		_putchar(i[counter]);
+		counter++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdio.h>
+/**
+ * _putchar - printing 
+ * @x :  recive value to print
+*/
+void _putchar(char x)
+{
+	putchar(x);
+}
