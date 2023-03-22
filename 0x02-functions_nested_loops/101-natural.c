38ad331315b5f05f4a5825b00ae34ad336b6928b
abdullah <abdallahfaik80@gmail.com>
2023-03-22 16:47:54 +0200

    task number 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - printing
+ * Return: 0
+*/
+int main(void)
+{
+	int j = 3;
+	int i = 5;
+	long int sum = 0;
+	int q = 1024;
+
+	while (j <= 1024)
+	{
+		sum = sum + j;
+		j += 3;
+	}
+	while (i <= 1024)
+	{
+		sum = sum + i;
+		i += 5;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
