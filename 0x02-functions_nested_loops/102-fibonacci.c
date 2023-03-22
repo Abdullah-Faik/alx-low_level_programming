e804afb1c1433f71faf15b0994cab39bcdeb0a58
abdullah <abdallahfaik80@gmail.com>
2023-03-22 19:36:35 +0200

    task number 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,15 +5,15 @@
 */
 int main(void)
 {
-	int i = 1;
-	int j = 0;
-	long int fibo = 1;
+	long long int i = 1;
+	long long int j = 0;
+	long long int fibo = 1;
 	int counter = 0;
 
 	while (counter < 50)
 	{
 		fibo = i + j;
-		printf("%ld", fibo);
+		printf("%lld", fibo);
 		j = i;
 		i = fibo;
 		counter++;
