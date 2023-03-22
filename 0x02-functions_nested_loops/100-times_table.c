a7d6f90ba6e11849dc76b22d959721056eae46cf
abdullah <abdallahfaik80@gmail.com>
2023-03-22 16:00:28 +0200

    task number 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * print_times_table - print table
+ * @n: recive numbers
+*/
+void print_times_table(int n)
+{
+	int k;
+	int i = 1;
+	int j = 1;
+
+	if (n < 15 && n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('0');
+			_putchar(' ');
+			_putchar(' ');
+			j = 1;
+			while (j < n)
+			{
+				k = i * j;
+				if (k >= 100)
+				{
+					_putchar('0' + k / 100);
+					_putchar('0' + ((k % 100) / 10));
+				}
+				else if (k > 10 && k < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + ((k % 100) / 10));
+
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar('0' + k % 10);
+				_putchar(',');
+				_putchar(' ');
+				j++;
+			}
+			i++;
+			_putchar('\n');
+		}
+	}
+}
