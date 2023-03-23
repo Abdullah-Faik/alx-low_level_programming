de6f4611259474aab5827c97454339618c475a2d
abdullah <abdallahfaik80@gmail.com>
2023-03-23 22:27:52 +0200

    task number 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - print numbers
+ * @size: recive numbers
+*/
+void print_triangle(int size)
+{
+	if (size >= 0)
+	{
+		int i = 1;
+		int j = 0;
+		int k = 0;
+
+		while (i <= size)
+		{
+			j = 0;
+			while (j < size - i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			k = 0;
+			while (k < i)
+			{
+				_putchar('#');
+				k++;
+			}
+			i++;
+			_putchar('\n');
+		}
+	}
+}
