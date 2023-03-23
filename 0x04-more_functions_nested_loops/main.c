558c8c222094ecf6e48da4867e475f16e457e412
abdullah <abdallahfaik80@gmail.com>
2023-03-23 19:24:35 +0200

    task number 7

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -4,7 +4,12 @@
  * _putchar - printing 
  * @x :  recive value to print
 */
-int mian(void)
+int main(void)
 {
-	
+	print_diagonal(5);
+	print_diagonal(0);
+	print_diagonal(-7);
+	print_diagonal(11);
+
+	return (0);
 }
