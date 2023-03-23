bc671f94a0cbfb7ae2d760c65ee5371455e9232b
abdullah <abdallahfaik80@gmail.com>
2023-03-23 17:11:27 +0200

    task number 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagonal - print numbers
+ * @n: recive numbers
+*/
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i <= n)
+	{
+		j = 0;
+		while (j < i)
+		{
+			_putchar(' ');
+		}
+	}
+	_putchar('\\');
+	_putchar('\n');
+}
