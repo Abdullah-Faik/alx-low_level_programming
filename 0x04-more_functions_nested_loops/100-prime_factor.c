d093ef32a9335560328f25e139d99b3c8299b64e
abdullah <abdallahfaik80@gmail.com>
2023-03-24 15:15:12 +0200

    task number 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - printing
+ * Return: 0
+*/
+int main(void)
+{
+	long int x = 612852475143;
+	int j = 2;
+	int i = 0;
+
+	while (i < x / 2)
+	{
+		while (x % j == 0)
+		{
+			x = x / j;
+			printf("%d\n", j);
+		}
+		j++;
+		i++;
+	}
+	printf("%ld\n", x);
+	return (0);
+}
