7e601486b88d2bc83167d0a05f3f19ff54296309
abdullah <abdallahfaik80@gmail.com>
2023-03-28 01:45:20 +0200

    task number 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,14 @@
 #include "main.h"
 #include <string.h>
 /**
- * 
+ * _strcpy - copy
+ * @dest: resive
+ * @src: recive
+ * Return: array
 */
 
 char *_strcpy(char *dest, char *src)
 {
-        strcpy(dest,src);
-        return dest;
+	strcpy(dest, src);
+	return (dest);
 }
