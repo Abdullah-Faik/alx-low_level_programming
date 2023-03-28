208ecbc17cfaea30056c07d142442a02d7221bc9
abdullah <abdallahfaik80@gmail.com>
2023-03-29 01:37:49 +0200

    task number 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _atoi - transform swtring to int
+ * @s: recive
+ * Return: sign int
+*/
+int _atoi(char *s)
+{
+	int k, sign, l, i;
+
+	l = strlen(s);
+	k = atoi(s);
+	for (i = 0; i < l; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+			break;
+	}
+	return (sign * k);
+
+}
