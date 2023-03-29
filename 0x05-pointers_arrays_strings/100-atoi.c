35992230d4fa55d9771e4df39ab19f53cf80f632
abdullah <abdallahfaik80@gmail.com>
2023-03-29 02:38:14 +0200

    task number 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,27 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+/**
+ * convert - convert number into nint
+ * @c: recive
+ * Return: the number
+*/
+int convert(char *c)
+{
+	int i = 0, sign = 0, l, r = 0;
+
+	l = strlen(c);
+	for (i = 0; i < l; i++)
+	{
+		if (c[i] >= '0' && c[i] <= '9')
+		{
+			r = r * 10 + (c[i] - '0');
+			sign = 1;
+		}
+		else if (sign)
+			break;
+	}
+	return (r);
+}
 /**
  * _atoi - transform swtring to int
  * @s: recive
@@ -10,13 +32,14 @@ int _atoi(char *s)
 	int k, sign = 1, l, i;
 
 	l = strlen(s);
-	k = atoi(s);
-	for (i = 0; i < l; i++)
+	k = convert(s);
+	while ((s[i] < '0' || i > s[i] > '9') && i < l)
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		if (s[i] >= '0' && s[i] <= '9')
 			break;
+		i++;
 	}
 	return (sign * k);
 
