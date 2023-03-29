5b368bae1d9645eadc636d3b75620960ae7cb119
abdullah <abdallahfaik80@gmail.com>
2023-03-29 03:57:04 +0200

    task number 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,22 +9,22 @@
  */
 int convert(char *c)
 {
-    int i = 0, sign = 0, l, r = 0;
+	int i = 0, sign = 0, l, r = 0;
 
-    l = strlen(c);
-    for (i = 0; i < l; i++)
-    {
-	if (c[i] >= '0' && c[i] <= '9')
+	l = strlen(c);
+	for (i = 0; i < l; i++)
 	{
-	       if (r > INT_MAX / 10 || (r == INT_MAX / 10 && c[i] - '0' > INT_MAX % 10))
-			return INT_MAX;
-	    	r = r * 10 + (c[i] - '0');
-	    	sign = 1;
-	}
+		if (c[i] >= '0' && c[i] <= '9')
+		{
+			if (r > INT_MAX / 10 || (r == INT_MAX / 10 && c[i] - '0' > INT_MAX % 10))
+				return (INT_MAX);
+			r = r * 10 + (c[i] - '0');
+			sign = 1;
+		}
 	else if (sign)
-	    break;
-    }
-    return (r);
+		break;
+	}
+	return (r);
 }
 
 /**
@@ -34,21 +34,19 @@ int convert(char *c)
  */
 int _atoi(char *s)
 {
-    	int k, sign = 1, l, i = 0;
+	int k, sign = 1, l, i = 0;
 
-    	l = strlen(s);
-   	 k = convert(s);
-    	while ((s[i] < '0' || s[i] > '9') && i < l)
-  	{
+	l = strlen(s);
+	k = convert(s);
+	while ((s[i] < '0' || s[i] > '9') && i < l)
+	{
 		if (s[i] == '-')
-		    sign *= -1;
+			sign *= -1;
 		if (s[i] >= '0' && s[i] <= '9')
-	  	  break;
+			break;
 		i++;
-    	}
-
-
-    	if (sign == -1 && k == INT_MAX)
-		return INT_MIN;
-   	 return (sign * k);
+	}
+	if (sign == -1 && k == INT_MAX)
+		return (INT_MIN);
+	return (sign * k);
 }
