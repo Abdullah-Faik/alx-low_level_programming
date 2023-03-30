08f396a8c64ad90126c277b28ed8274ef90c734d
abdullah <abdallahfaik80@gmail.com>
2023-03-30 21:31:30 +0200

    task number 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+/**
+ * _strcmp - compair
+ * @s1: recive
+ * @s2: recive
+ * Return: result
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	x = strcmp(s1, s2);
+	return (x);
+}
