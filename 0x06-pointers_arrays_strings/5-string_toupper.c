8b94bfb8f1f068f5423b9f5ed5e95d9b949ad79f
abdullah <abdallahfaik80@gmail.com>
2023-03-30 21:55:40 +0200

    task number 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+/**
+ * string_toupper - upper case
+ * @s: recive
+ * Return: edited string
+*/
+char *string_toupper(char *s)
+{
+	int q = 32, i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] + q;
+		i++;
+	}
+	return (s);
+}
