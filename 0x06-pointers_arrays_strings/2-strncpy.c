e784fc216dd58049d78645a322912670c27fcfbc
abdullah <abdallahfaik80@gmail.com>
2023-03-30 21:21:26 +0200

    task number 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 /**
  * _strncpy - copying
  * @dest: recive
@@ -9,9 +10,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	char q[100];
-
-	strcpy(q, src);
-	q[n] = '\0';
-	strcpy(dest, q);
+	int i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
 }
