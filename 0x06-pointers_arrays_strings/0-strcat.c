9e93346e624c85923c72d23832ba1c4c06be6759
abdullah <abdallahfaik80@gmail.com>
2023-03-30 20:11:48 +0200

    task number 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * _strcat - contatination
+ * @dest: recive
+ * @src: recive
+ * Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
