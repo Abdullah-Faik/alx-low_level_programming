91c60dd99aa1307fdb28d9cb48bdd9f53f047e07
abdullah <abdallahfaik80@gmail.com>
2023-03-30 20:43:46 +0200

    task number 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,11 +4,13 @@
  * _strncat - contatination
  * @dest: recive
  * @src: recive
+ * @n: recive
  * Return: dest
 */
 char *_strncat(char *dest, char *src, int n)
 {
 	char q[100];
+
 	strcpy(q, src);
 	q[n] = '\0';
 	strcat(dest, q);
