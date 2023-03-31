cdb809ae0f980cdd1b47d3fedf13685f63aa9889
abdullah <abdallahfaik80@gmail.com>
2023-03-31 06:26:10 +0200

    task number 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+
+/**
+ * rot13 - check the code
+ * @s: recive
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char o[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j = 0, k;
+
+	while (s[j] != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (s[j] == c[i])
+			{
+				s[j] = o[i];
+				break;
+			}
+		}
+		j++;
+	}
+	return (s);
+}
