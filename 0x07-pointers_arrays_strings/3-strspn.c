57c141fea08b18c748ab5c2051b98de11b37dd54
abdullah <abdallahfaik80@gmail.com>
2023-04-03 15:48:38 +0200

    task number 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to search
+ * @accept: the characters to accept in the prefix
+ *
+ * Return: the number of bytes in the initial segment of s which
+ * consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+
+	while (*s != '\0')
+	{
+		char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				count++;
+				break;
+			}
+			a++;
+		}
+		if (*a == '\0')
+		{
+			break;
+		}
+		s++;
+	}
+	return (count);
+}
