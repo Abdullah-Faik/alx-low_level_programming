b5e8f12737213267a6baa9c98f525146713dcabb
abdullah <abdallahfaik80@gmail.com>
2023-04-03 07:48:27 +0200

    Edit whoole

---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1, b = 2, c, count = 2;
+    printf("%d, %d, ", a, b);
+    while (count < 98) {
+        c = a + b;
+        printf("%d, ", c);
+        a = b;
+        b = c;
+        count++;
+    }
+    printf("%d\n", a+b);
+    return 0;
+}
--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 /**
  * print_number - Prints an integer to stdout.
  *
@@ -9,12 +10,26 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n = -n;
 	}
 	if (n / 10 != 0)
 	{
 		print_number(n / 10);
 	}
-	_putchar(n % 10 + '0');
+	putchar(n % 10 + '0');
+}
+int main(void)
+{
+    print_number(98);
+    putchar('\n');
+    print_number(402);
+    putchar('\n');
+    print_number(1024);
+    putchar('\n');
+    print_number(0);
+    putchar('\n');
+    print_number(-98);
+    putchar('\n');
+    return (0);
 }
