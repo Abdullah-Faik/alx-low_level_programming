f90c6b5f127bfa9a0e5b207e734abec7c10301cc
abdullah <abdallahfaik80@gmail.com>
2023-04-05 02:33:38 +0200

    task number 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-print_diagsums - Calculates the sum of the two diagonals of a square matrix.
+* print_diagsums - Calculates the sum of the two diagonals of a square matrix.
 * @a: Pointer to the first element of the matrix.
 * @size: Size of the matrix.
 * Return: void.
@@ -10,51 +10,12 @@ void print_diagsums(int *a, int size)
 {
 	int sum1 = 0;
 	int sum2 = 0;
-	int i;
-	int digit;
-	for (i = 0; i < size * size; i += size + 1)
+	int i, j;
+
+	for (i = 0, j = 1; i < size, j <= size; i++, j++)
 	{
-		sum1 += *(a + i);
+		sum1 = sum1 + a[i + i * size];
+		sum2 = sum2 + a[(size - 1) * j];
 	}
-
-	for (i = size - 1; i < size * size - 1; i += size - 1)
-	{
-		sum2 += *(a + i);
-	}
-
-	printf("Diagonal sum 1: ");
-
-
-	if (sum1 == 0)
-	{
-		putchar('0');
-	}
-	else
-	{
-		while (sum1 > 0)
-		{
-			digit = sum1 % 10;
-			_putchar(digit + '0');
-			sum1 /= 10;
-		}
-	}
-
-	putchar('\n');
-	printf("Diagonal sum 2: ");
-
-	if (sum2 == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		while (sum2 > 0)
-		{
-			digit = sum2 % 10;
-			_putchar(digit + '0');
-			sum2 /= 10;
-		}
-	}
-
-	putchar('\n');
+	printf("%d, %d\n", sum1, sum2);
 }
