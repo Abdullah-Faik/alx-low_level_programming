edac1f6b6f146867848d04f9f10aef1689fbcd7d
abdullah <abdallahfaik80@gmail.com>
2023-04-08 02:03:04 +0200

    4

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int q, w, e, r, t;
+
+	if (argc < 2)
+	{
+		printf("Error");
+		return (1);
+	}
+	argc = argc - 1;
+	q = argc / 25;
+	argc = argc % 25;
+	w = argc / 10;
+	argc = argc % 10;
+	e = argc / 5;
+	argc = argc % 5;
+	r = argc / 2;
+	argc = argc % 2;
+	t = argc / 1;
+	printf("%d", q + w + e + r + t);
+	return (0);
+}
