4627be9f124c728a57b676ae3750e08449dcfcfd
abdullah <abdallahfaik80@gmail.com>
2023-04-17 20:29:41 +0200

    task number 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _my_dog_h
+#define _my_dog_h
+/**
+ * struct dog - struct dog
+ * @name: name of dog
+ * @owner: owner of dog
+ * @age: age of dog
+ *
+ * Description: struct dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
