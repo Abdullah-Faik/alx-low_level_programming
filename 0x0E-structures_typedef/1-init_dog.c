c5c7a185db8a044688ec5bc09d0da961ee948d61
abdullah <abdallahfaik80@gmail.com>
2023-04-17 20:49:12 +0200

    task number 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stdlib.h>
 /**
  * init_dog - initialize members of dog
  * @d: pointer to struct dog to modify members of
@@ -9,7 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
