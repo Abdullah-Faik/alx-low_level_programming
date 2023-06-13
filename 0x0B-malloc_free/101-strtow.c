7ca5256fb1e6afb68b738e0d35964ad111558182
abdullah <abdallahfaik80@gmail.com>
2023-06-13 22:19:06 +0300

    last file

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,46 +9,46 @@
  */
 char **strtow(char *str)
 {
-    int i = 0, j = 0, k = 0, l = 0, m = 0;
-    char **arr;
+	int i = 0, j = 0, k = 0, l = 0, m = 0;
+	char **arr;
 
-    if (str == NULL || str == "")
-        return (NULL);
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] != ' ')
-            l += 1;
-    }
-    arr = malloc(sizeof(char *) * (l + 1));
-    if (arr == NULL)
-        return (NULL);
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] != ' ')
-        {
-            for (j = i; str[j] != ' ' && str[j] != '\0'; j++)
-            {
-                k += 1;
-            }
-            arr[m] = malloc(sizeof(char) * (k + 1));
-            if (arr[m] == NULL)
-            {
-                for (j = 0; j < m; j++)
-                    free(arr[j]);
-                free(arr);
-                return (NULL);
-            }
-            for (j = 0; j < k; j++)
-            {
-                arr[m][j] = str[i];
-                i++;
-            }
-            arr[m][j] = '\0';
-            m++;
-            k = 0;
-        }
-    }
-    arr[m] = NULL;
-    return (arr);
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+			l += 1;
+	}
+	arr = malloc(sizeof(char *) * (l + 1));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+			for (j = i; str[j] != ' ' && str[j] != '\0'; j++)
+			{
+				k += 1;
+			}
+			arr[m] = malloc(sizeof(char) * (k + 1));
+			if (arr[m] == NULL)
+			{
+				for (j = 0; j < m; j++)
+					free(arr[j]);
+				free(arr);
+				return (NULL);
+			}
+			for (j = 0; j < k; j++)
+			{
+				arr[m][j] = str[i];
+				i++;
+			}
+			arr[m][j] = '\0';
+			m++;
+			k = 0;
+		}
+	}
+	arr[m] = NULL;
+	return (arr);
 
 }
