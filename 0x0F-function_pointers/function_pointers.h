5aa4b4847c5e0f98cc854e8aac3fb2eaee8b1a99
abdullah <abdallahfaik80@gmail.com>
2023-06-22 01:24:06 +0300

    task number 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
 #ifndef function_pointers_h
 #define function_pointers_h
+#include <stdlib.h>
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
