51870679314bf6b60253cb34db0ecd5a57f15c7b
abdullah-faik <abdallahfaik80@gmail.com>
2023-07-07 04:50:07 +0300

    task number 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,29 +4,41 @@
  * @n: number to print in binary
  * Return: void
  */
-
 void print_binary(unsigned long int n)
 {
-	unsigned long int k = 512 << 15;
-	int f = 0;
-
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	while (k > 0)
-	{
-		if (n >= k)
-		{
-			f = 1;
-			_putchar('1');
-			n -= k;
-		}
-		else
-		{
-			if (f == 1)
-			_putchar('0');
-		}
-		k = k >> 1;
-	}
+	if (n >> 1)
+		print_binary(n >> 1);
+	/*n & 1 ? printf("1") : printf("0");*/
+	n & 1 ? _putchar('1') : _putchar('0');
 }
+
+
+/*
+* this my sol
+*{
+*	unsigned long int i = 0, j = 0, k = 64;
+*	int f = 0;
+*	int res = 0;
+*void print_binary(unsigned long int n)
+*	while (n > 0)
+*	{
+*		k = k >> 1;
+*		res = n - k;
+*		printf("%d\n", res);
+*		if (res >= 0)
+*		{
+*			f = 1;
+*			printf("1");
+*			_putchar('1');
+*		}
+*		else
+*		{
+*			n = n + k;
+*			if (f == 1)
+*			   printf("0");
+*			     _putchar('0');
+*		}
+*		printf("%lu\n", n);
+*	}
+*}
+*/
