021f558bfe77c8e25164f950b2e7ea9609829f66
abdullah-faik <abdallahfaik80@gmail.com>
2023-07-07 04:10:59 +0300

    task number 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,35 @@
+#include "main.h"
 
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print in binary
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int k = 512 << 10;
+	int f = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	while (k > 0)
+	{
+		if (n >= k)
+		{
+			f = 1;
+			printf("1");
+			_putchar('1');
+			n -= k;
+		}
+		else
+		{
+			if (f == 1)
+				printf("0");
+			_putchar('0');
+		}
+		k = k >> 1;
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 int set_bit(unsigned long int *n, unsigned int index);
+void print_binary(unsigned long int n);
 #endif
