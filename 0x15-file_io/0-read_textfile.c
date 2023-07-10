49d53d5f1ff85a2b8c50d4133d7d2bcf8bced9a9
abdullah-faik <abdallahfaik80@gmail.com>
2023-07-10 15:44:03 +0300

    task number 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,40 @@
 #include "main.h"
-/**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: file to read
- * @letters: number of letters it should read and print
- * Return: number of letters it could read and print
-*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t i = 0;
+	ssize_t i = 0;
 	int o;
 	FILE *file;
 	char *text;
 
 	o = open(filename, O_RDONLY);
-	if (!*filename || o == - 1)
-		return (0);
+	if (!filename || o == -1)
+		return 0;
 
 	file = fdopen(o, "r");
-	if (file == 0)
-		return (0);
+	if (file == NULL)
+		return 0;
 
 	text = (char *)malloc(letters + 1);
 	if (text == NULL)
 	{
 		fclose(file);
-		return (0);
+		return 0;
 	}
 
 	i = fread(text, sizeof(char), letters, file);
 
-	if (i > letters)
+	if (i != (ssize_t)letters)
 	{
 		free(text);
 		fclose(file);
-		return (0);
+		return 0;
 	}
-	o = 0;
-	o += write(1, text, i);
 
-	if (i < letters)
-		o += write(1, "\n", 1);
+	write(1, text, i);
+
 	free(text);
 	fclose(file);
-	return (o);
+	
+	return i;
 }
