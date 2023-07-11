e120b8b820eea516c373f42857e298eecbe5a55a
abdullah-faik <abdallahfaik80@gmail.com>
2023-07-11 17:00:36 +0300

    task number 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,51 +6,32 @@
 #include <unistd.h>
 #include <string.h>
 /**
- * main - copies the content of a file to another file
- * @args: number of arguments
- * @argv: arguments
-*/
+ * copy_file_help - copies the content of a file to another file
+ * @fd: file descriptor
+ * @file_from: file to copy from
+ * @file_to: file to copy to
+ * @fd2: file descriptor
+ * @rd: read
+ * @buffer: buffer
+ */
 
-int main(int args , char *argv[])
+void copy_file_help(int fd, char *file_from, char *file_to,
+int fd2, int rd, char *buffer)
 {
-	int fd, fd2, rd, wr;
-	char *buffer[1024];
+	int wr;
 
-	if (args != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	rd = read(fd, buffer, 1024);
-	if (rd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	while (rd > 0)
 	{
 		wr = write(fd2, buffer, rd);
 		if (wr == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			exit(99);
 		}
 		rd = read(fd, buffer, 1024);
 		if (rd == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			exit(98);
 		}
 	}
@@ -64,5 +45,54 @@ int main(int args , char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
+}
+
+/**
+ * copy_file - copies the content of a file to another file
+ * @file_from: file to copy from
+ * @file_to: file to copy to
+ */
+void copy_file(char *file_from, char *file_to)
+{
+	int fd, fd2, rd;
+	char buffer[1024];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	fd = open(file_from, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	rd = read(fd, buffer, 1024);
+	if (rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	copy_file_help(fd, file_from, file_to, fd2, rd, buffer);
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @args: number of arguments
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int args, char *argv[])
+{
+
+	if (args != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	copy_file(argv[1], argv[2]);
 	return (0);
 }
