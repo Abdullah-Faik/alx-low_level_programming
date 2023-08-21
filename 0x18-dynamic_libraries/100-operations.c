a5f55350b2ac9482c535dc7d411996ab372ac74e
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-08-21 12:28:55 +0300

    adding_library

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include "100-operations.h"
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - sub two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - mul two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - div two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - mod two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
