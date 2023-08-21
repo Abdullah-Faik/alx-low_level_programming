93b2ce288101319ec19675fcfbcd8c1362db847d
Abdullah-Faik <abdallahfaik80@gmail.com>
2023-08-21 12:50:56 +0300

    adding_library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-# Modify the output of ls and md5sum
-echo "gm" > ls
-echo "d52e6c18e0723f5b025a75dea19ef365  gm" > md5sum
-
-# Run the Giga Millions program with modified output
-./gm 9 8 10 24 75 9
+echo -e '#!/bin/bash\necho "--> Please make me win!"' > gm
+chmod +x gm
