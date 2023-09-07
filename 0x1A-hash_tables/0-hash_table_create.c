633a83fe7ca88044dc73f7b3ab70f58131e2a16b
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-07 18:00:38 +0300

    update_betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,24 +1,31 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - function that creates a hash table.
+ * @size: size of the array
+ * Return: a pointer to the newly created hash table
+ * If something went wrong, the function should return NULL
+ * Description: the function should return NULL if size is set to 0
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *taple = malloc(sizeof(hash_table_t));
-    unsigned long int i;
-    if (taple == NULL)
-    {
-        return (NULL);
-    }
+	hash_table_t *taple = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
-    taple->size = size;
-    taple->array = malloc(sizeof(hash_node_t *) * size);
+	if (taple == NULL)
+	{
+		return (NULL);
+	}
 
-    if (taple->array == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; i < size; i++)
-    {
-        taple->array[i] = NULL;
-    }
-    return (taple);
+	taple->size = size;
+	taple->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (taple->array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		taple->array[i] = NULL;
+	}
+	return (taple);
 }
--- 0x1A-hash_tables/main_helper.c
+++ 0x1A-hash_tables/main_helper.c
@@ -1,26 +1,31 @@
 #include "hash_tables.h"
-
+/**
+ * free_table - function that frees a hash table.
+ * @ht: hash table
+ * Return: void
+ * Description: the function should free the table
+*/
 void free_table(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *tmp;
-    hash_node_t *tmp2;
+	unsigned long int i;
+	hash_node_t *tmp;
+	hash_node_t *tmp2;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        tmp = ht->array[i];
-        while (tmp != NULL)
-        {
-            tmp2 = tmp->next;
-            free(tmp->key);
-            free(tmp->value);
-            free(tmp);
-            tmp = tmp2;
-        }
-    }
-    free(ht->array);
-    free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			tmp2 = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = tmp2;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
