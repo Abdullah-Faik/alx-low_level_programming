033f45baa8d9500235844bba873d6fe133aab56d
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-07 20:16:13 +0300

    fix gcc error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,21 +1,28 @@
 #include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table to add or update the key/value to
+ * @key: key to add to the hash table
+ * @value: value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int indx, i;
+	unsigned long int indx;
 	hash_node_t *node;
 
 	if (ht == NULL || key == NULL || value == NULL ||
-			ht->size == 0 || ht->array == NULL)
+		ht->size == 0 || ht->array == NULL)
 	{
 		return (0);
 	}
-	indx = key_index(key, ht->size);
+	indx = key_index((const unsigned char *)key, ht->size);
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 	{
 		return (0);
 	}
-	node->key =  strdup(key);
+	node->key = strdup(key);
 	node->value = strdup(value);
 	if (ht->array[indx] == NULL)
 	{
