26c9575f5934e3ae04795e249576256558c0d329
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-07 18:30:29 +0300

    fix betty

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,13 +7,13 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int indx;
+	unsigned long int indx;
 
-    if (key == NULL || size == 0)
-        return (0);
+	if (key == NULL || size == 0)
+		return (0);
 
-    indx = hash_djb2(key);
-    indx = indx % size;
+	indx = hash_djb2(key);
+	indx = indx % size;
 
-    return (indx);
+	return (indx);
 }
