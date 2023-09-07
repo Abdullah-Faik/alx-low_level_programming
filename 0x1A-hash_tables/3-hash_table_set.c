17476fc617e43262cad06e1f949e5037e418619e
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-07 20:25:26 +0300

    fix tasks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,18 +10,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int indx;
 	hash_node_t *node;
+	hash_node_t *temp;
 
 	if (ht == NULL || key == NULL || value == NULL ||
 		ht->size == 0 || ht->array == NULL)
-	{
 		return (0);
-	}
 	indx = key_index((const unsigned char *)key, ht->size);
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
-	{
 		return (0);
-	}
 	node->key = strdup(key);
 	node->value = strdup(value);
 	if (ht->array[indx] == NULL)
@@ -31,6 +28,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
+		temp = ht->array[indx];
+		while (temp->next != NULL)
+		{
+			if (strcmp(temp->key, key) == 0)
+			{
+				free(temp->value);
+				temp->value = strdup(value);
+				free(node->key);
+				free(node->value);
+				free(node);
+				return (1);
+			}
+			temp = temp->next;
+		}
 		node->next = ht->array[indx];
 		ht->array[indx] = node;
 	}
