f4b55e04a29e392f0b2375051728eb55966b704a
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-08 11:18:33 +0300

    task num 6 betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,25 +7,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *tmp;
-    hash_node_t *tmp2;
+	unsigned long int i;
+	hash_node_t *tmp;
+	hash_node_t *tmp2;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        tmp = ht->array[i];
-        while (tmp != NULL)
-        {
-            tmp2 = tmp->next;
-            free(tmp->key);
-            free(tmp->value);
-            free(tmp);
-            tmp = tmp2;
-        }
-    }
-    free(ht->array);
-    free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			tmp2 = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = tmp2;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
